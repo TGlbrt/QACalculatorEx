646dd1301b4c90b8c24f46990bf1447c784464df
TGilbert <Thomas.Gilbert@academytrainee.com>
2019-05-29 12:16:02 +0100

    created functions to handle power feature, extending the functionality of Function to retrieve the value

--- src/main/java/com/qa/calculator/Functions.java
+++ src/main/java/com/qa/calculator/Functions.java
@@ -4,6 +4,6 @@ public interface Functions {
 
 	public void setValue(int numOne, int numTwo);
 	public void setValue(double numOne, double numTwo);
-	public int getValue();
-	public double getValueDouble();
+	public double getValue();
+	
 }
--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -1,24 +1,49 @@
 package com.qa.calculator;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
-Calculator calc; 
+	Calculator calc; 
+
+	@Before
+	public void setup() {
+		calc = new Calculator();
+	}
+	
+	@After
+	public void teardown() {
+		calc.clear();
+	}
+	
 	@Test
 	public void addition() {
-		calc = new Calculator();
+		
 		int actualValue = calc.add(2, 2);
 		Assert.assertEquals(4, actualValue);
 	}
 	
 	@Test
 	public void testSubtraction() {
-		Calculator aCalc = new Calculator();
-		int storedValue = aCalc.sub(4,2);
-		double anotherValue = aCalc.sub(4.5,2.4);
+		int storedValue = calc.sub(4,2);
+		double anotherValue = calc.sub(4.5,2.4);
+		calc.setValue(4.5, 2.4);
+		anotherValue = calc.sub();
 		Assert.assertEquals(2, storedValue);
 		Assert.assertEquals(2.1, anotherValue,0.1);
+	}
+	
+	@Test
+	public void testPower() {
+		calc.setValue(4,2);
+		double returnedValue = calc.power();
+		Assert.assertEquals(16, returnedValue,0.1);
+		calc.clear();
+		calc.setValue(4, 3);
+		returnedValue = calc.power();
+		Assert.assertEquals(64, returnedValue,0.1);
 	}
 
 }
