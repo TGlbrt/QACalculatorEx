1c149c4fe2c67eabff803935d13910d55846e270
AChar95 <allancharles95@outlook.com>
2019-05-29 11:26:27 +0100

    Added interface to calculator

---
+++ src/main/java/com/qa/calculator/Functions.java
@@ -0,0 +1,9 @@
+package com.qa.calculator;
+
+public interface Functions {
+
+	public void setValue(int numOne, int numTwo);
+	public void setValue(double numOne, double numTwo);
+	public int getValue();
+	public double getValueDouble();
+}
--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -4,10 +4,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
-
+Calculator calc; 
 	@Test
-	public void test() {
-		Calculator calc = new Calculator();
+	public void addition() {
+		calc = new Calculator();
 		int actualValue = calc.add(2, 2);
 		Assert.assertEquals(4, actualValue);
 	}
